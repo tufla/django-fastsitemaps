94c35cda87d8a9076c858954813de936a929bfb4
@tufla <henry@oppten.com>
2014-01-10 14:41:03 -0300

    Merging with original fork

--- fastsitemaps/generator.py
+++ fastsitemaps/generator.py
@@ -16,7 +16,6 @@ def sitemap_generator(request, maps, page, current_site):
     yield output.getvalue()
     pos = output.tell()
     for site in maps:
-        site.request = request
         if callable(site):
             if issubclass(site, RequestSitemap):
                 site = site(request=request)
